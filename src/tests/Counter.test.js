2e86a3ed729b8e5db39274314b133330704f282b
Ashish Satyava <58479500+asatyavarapu@users.noreply.github.com>
2022-12-04 16:16:11 -0500

    Update Counter.test.js

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,5 +1,5 @@
 // import necessary react testing library helpers here
-import {render, screen} from '@testing-library/react';
+import {render, screen, getByTestId } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 // import the Counter component here
 import Counter from '../components/Counter';
@@ -11,24 +11,32 @@ beforeEach(() => {
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
-  const counterName = screen.getByText(/Counter/i);
-  expect(counterName).toBeInTheDocument;
+  const counter = screen.getByText(/Counter/i);
+  expect(counter).toBeInTheDocument;
 });
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
-  const count = screen.getByText(/0/i);
-  expect(count) = 0;
+  const counter = screen.getByTestId("count");
+  expect(counter).toHaveTextContent("0");
 });
 
 test('clicking + increments the count', () => {
   // Complete the unit test below based on the objective in the line above
-  const count = screen.getByText(/1/i);
-  expect(count) = 1;
+  const plus = screen.getByRole('button', {name: '+'});
+  const counter = screen.getByTestId("count");
+  expect(counter).toHaveTextContent("0");
+  userEvent.click(plus);
+  expect(counter).toHaveTextContent("1");
 });
 
 test('clicking - decrements the count', () => {
   // Complete the unit test below based on the objective in the line above
-  const count = screen.getByText(/0/i);
-  expect(count) = 0;
+  const plus = screen.getByRole('button', {name: '+'});
+  const minus = screen.getByRole('button', {name: '-'});
+  const counter = screen.getByTestId("count");
+  userEvent.click(plus);
+  expect(counter).toHaveTextContent("1");
+  userEvent.click(minus);
+  expect(counter).toHaveTextContent("0");
 });
